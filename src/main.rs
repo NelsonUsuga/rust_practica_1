49f39990b69d8d56d475b9b7fe6bf9efa5b4f2b9
Nelson Usuga <nelsonusuga@erpsolucionesweb.com>
2019-08-27 17:12:02 -0500

    Signed-off-by: Nelson Usuga <nelsonusuga@erpsolucionesweb.com>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::{clap_app, crate_version};
 
 fn main() {
     let _clap = clap_app!(
-		mdrend =>
+		rust_practica_1 =>
 		(version:crate_version!())
 		(author:"Nelson Úsuga")
 		(about:"Curso de rust")
